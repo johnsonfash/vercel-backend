12bbd02bd9f2b3ac4d752263e8398896eaa8a082
johnsonfash <fashanutosin7@gmail.com>
2024-10-28 07:22:40 +0100

    removed Ds store

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import serverless from 'serverless-http';
 
 const app = express();
 app.use(express.json());
+
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello from Express with TypeScript on Vercel!' });
 });
+
 module.exports = app;
 module.exports.handler = serverless(app); 
