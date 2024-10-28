4d1497844480cf8104223a2f0a699aafcb40c7b6
johnsonfash <fashanutosin7@gmail.com>
2024-10-28 08:56:30 +0100

    added vercel config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import serverless from 'serverless-http';
 import app from './app';
 
-module.exports = app;
-module.exports.handler = serverless(app); 
-// For local testing
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+const isVercel = process.env.VERCEL === '1';
+if (isVercel) {
+    module.exports = app;
+    module.exports.handler = serverless(app);
+} else {
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
