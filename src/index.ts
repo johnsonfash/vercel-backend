fdcbf15a449d4b5ec78ea551c71e48a005b84e18
johnsonfash <fashanutosin7@gmail.com>
2024-10-28 08:44:18 +0100

    fixed path for images

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ import app from './app';
 
 module.exports = app;
 module.exports.handler = serverless(app); 
+// For local testing
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
