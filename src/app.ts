746f4f7387384936e319de9d4c73232c57022925
johnsonfash <fashanutosin7@gmail.com>
2024-10-28 08:37:47 +0100

    added static image:

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
 import path from 'path';
 
-// const __filename = path.join(process.cwd());
-// const __dirname = path.dirname(__filename);
-
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express with TypeScript on Vercel!', __filename, __dirname });
