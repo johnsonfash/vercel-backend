8e8f94c8ff8877752b5b17f7b6a607ff6cffedf5
johnsonfash <fashanutosin7@gmail.com>
2024-10-28 08:35:26 +0100

    q

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import path from 'path';
+
+// const __filename = path.join(process.cwd());
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express with TypeScript on Vercel!' });
+  res.json({ message: 'Hello from Express with TypeScript on Vercel!', __filename, __dirname });
 });
 
 export default app;
